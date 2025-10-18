758fc92b3b0544d5c185fb373fe335a535edd6e1
dustinwania <dustin.wania03@gmail.com>
2025-10-18 15:05:36 +0800

    added additional sites for navigation

---
+++ app/components/ImageCarousel.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+Carousel,
+CarouselContent,
+CarouselItem,
+} from "@/components/ui/carousel";
+
+import Autoplay from "embla-carousel-autoplay";
+
+export default function ImageCarousel() {
+    const autoplay = Autoplay({ delay: 2000, stopOnInteraction: false });
+    return(
+        <Carousel
+            className="h-[30vh]"
+            plugins={[
+                Autoplay({
+                delay: 2000,
+                }),
+            ]}
+            >
+                <CarouselContent>
+                {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="basis-full">
+                    <Card className="h-[30vh] border rounded-lg">
+                        <CardContent>
+                        <span className="text-5xl font-bold">{index + 1}</span>
+                        </CardContent>
+                    </Card>
+                    </CarouselItem>
+                ))}
+                </CarouselContent>
+        </Carousel>
+    )
+    }
